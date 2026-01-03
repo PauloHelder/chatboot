7a990484f3a38080f80541804c6bdaf02fe66772
Paulo Fernandes <37852449+PauloHelder@users.noreply.github.com>
2026-01-03 20:24:29 +0100

    Reformat app.py with consistent indentation

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def getall():
 def webhook():
     data = request.json
 
-    number = data['data']['key']['pushName']
+    number = data['data']['pushName']
 
     print(f'EVENTO RECEBIDO: {data}')
 
@@ -54,6 +54,7 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
