c0e15b462ceb173923fadcb7e05d86d380ef325a
Paulo Fernandes <37852449+PauloHelder@users.noreply.github.com>
2026-01-09 00:40:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def getall():
 def webhook():
     data = request.json
 
-    #number = data['data']['pushName']
-    number = '244973967181'
+    number = data['data']['remoteJid']
+    #number = '244973967181'
     print(f'EVENTO RECEBIDO: {data}')
 
    # waha = Waha()
@@ -55,6 +55,7 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
